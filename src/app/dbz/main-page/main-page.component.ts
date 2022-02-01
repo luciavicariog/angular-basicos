1d273d6f21ae7dfb64ac6a393830fef4b34a1c78
luciavicariog <luciavicariog@gmail.com>
2022-02-01 19:08:00 +0100

    Sección 5 completa

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+
+  nuevo : Personaje = {
+    nombre:'Yoda',
+    poder: 2000
+  }
+
+  // agregarNuevoPersonaje(evento: Personaje){
+  //   this.personajes.push(evento);
+  // }
+
+  constructor(private dbzService : DbzService){}
+
+}
